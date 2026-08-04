a5bfd1e63481542b22a0abdc4822b7ca6f57d99a
jaimito-asuntos-gringuenos <jaimito-asuntos-gringuenos@users.noreply.github.com>
2026-08-04 22:33:23 +0000

    Remove preview for PR 195 🛬

--- pr-preview/pr-195/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/OurHike/pr-preview/pr-195/sw.js', { scope: '/OurHike/pr-preview/pr-195/' })})}

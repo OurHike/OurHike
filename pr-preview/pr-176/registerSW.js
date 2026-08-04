df95a7b56e65ed564ec0d0a76975baa0e442f212
jaimito-asuntos-gringuenos <jaimito-asuntos-gringuenos@users.noreply.github.com>
2026-08-04 10:51:41 +0000

    Remove preview for PR 176 🛬

--- pr-preview/pr-176/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/OurHike/pr-preview/pr-176/sw.js', { scope: '/OurHike/pr-preview/pr-176/' })})}

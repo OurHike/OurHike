b2685b2cf178ec18a85eacf4c5a48b1f2c6ac670
jaimito-asuntos-gringuenos <jaimito-asuntos-gringuenos@users.noreply.github.com>
2026-08-05 19:11:00 +0000

    Deploy preview for PR 205 🛫

---
+++ pr-preview/pr-205/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/OurHike/pr-preview/pr-205/sw.js', { scope: '/OurHike/pr-preview/pr-205/' })})}

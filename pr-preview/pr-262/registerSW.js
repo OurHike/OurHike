606663925e392d90f48207a1841ca60387a8f397
jaimito-asuntos-gringuenos <jaimito-asuntos-gringuenos@users.noreply.github.com>
2026-08-06 12:00:57 +0000

    Remove preview for PR 262 🛬

--- pr-preview/pr-262/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/OurHike/pr-preview/pr-262/sw.js', { scope: '/OurHike/pr-preview/pr-262/' })})}
